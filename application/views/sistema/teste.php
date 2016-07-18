f3856fb955badb5d8072774e7785de99ff4f8ac8
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-07-18 15:45:51 -0300

    [INC] Testes de layout

--- application/views/sistema/teste.php
+++ application/views/sistema/teste.php
@@ -1,8 +1,62 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<p>
-<?php
-echo "Apenas um teste. " . $nome;
-?>
-</p>
+
+<div class="cabecalho expanded row">
+    <div class="medium-6 column">
+        Teste
+    </div>
+    <div class="medium-6 column">
+
+    </div>
+</div>
+<div class="corpo">
+    <div class="menu-esquerdo">
+        <div class="menu-esquerdo-conteudo">
+            <ul class="vertical menu">
+                <li><a href="#">Foundation</a></li>
+                <li><a href="#">Dot</a></li>
+                <li><a href="#">ZURB</a></li>
+                <li><a href="#">Com</a></li>
+                <li><a href="#">Slash</a></li>
+                <li><a href="#">Sites</a></li>
+                <li><a href="#">Foundation</a></li>
+                <li><a href="#">Dot</a></li>
+                <li><a href="#">ZURB</a></li>
+                <li><a href="#">Com</a></li>
+                <li><a href="#">Slash</a></li>
+                <li><a href="#">Sites</a></li>
+                <li><a href="#">Foundation</a></li>
+                <li><a href="#">Dot</a></li>
+                <li><a href="#">ZURB</a></li>
+                <li><a href="#">Com</a></li>
+                <li><a href="#">Slash</a></li>
+                <li><a href="#">Sites</a></li>
+                <li><a href="#">Foundation</a></li>
+                <li><a href="#">Dot</a></li>
+                <li><a href="#">ZURB</a></li>
+                <li><a href="#">Com</a></li>
+                <li><a href="#">Slash</a></li>
+                <li><a href="#">Sites</a></li>
+                <li><a href="#">Foundation</a></li>
+                <li><a href="#">Dot</a></li>
+                <li><a href="#">ZURB</a></li>
+                <li><a href="#">Com</a></li>
+                <li><a href="#">Slash</a></li>
+                <li><a href="#">Sites</a></li>
+                <li><a href="#">Foundation</a></li>
+                <li><a href="#">Dot</a></li>
+                <li><a href="#">ZURB</a></li>
+                <li><a href="#">Com</a></li>
+                <li><a href="#">Slash</a></li>
+                <li><a href="#">Sites</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="painel">
+        <div class="painel-conteudo row">
+            <p>Teste do painel de conteudo</p>
+        </div>
+        
+    </div>
+</div>

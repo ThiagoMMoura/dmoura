35103b394b5254a5381273f49b6aa1a2e4ffaf35
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2016-07-20 00:13:39 -0300

    [INC] Teste layout.

--- application/views/sistema/teste.php
+++ application/views/sistema/teste.php
@@ -1,66 +1,84 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<div class="dm-estrutura">
+    <header class="dm-cabecalho expanded row">
+        <div class="medium-6 column">
+            Teste
+        </div>
+        <div class="medium-6 column">
 
-<header class="dm-cabecalho expanded row">
-    <div class="medium-6 column">
-        Teste
-    </div>
-    <div class="medium-6 column">
-
-    </div>
-</header>
-<div class="-corpo expanded row">
-    <div id="dm-menu-esquerdo" class="column">
-        <div id="menu-esquerdo-interno">
-            <div id="menu-esquerdo-interno-interno">
-                <ul class="vertical menu">
-                    <li><a href="#">Foundation</a></li>
-                    <li><a href="#">Dot</a></li>
-                    <li><a href="#">ZURB</a></li>
-                    <li><a href="#">Com</a></li>
-                    <li><a href="#">Slash</a></li>
-                    <li><a href="#">Sites</a></li>
-                    <li><a href="#">Foundation</a></li>
-                    <li><a href="#">Dot</a></li>
-                    <li><a href="#">ZURB</a></li>
-                    <li><a href="#">Com</a></li>
-                    <li><a href="#">Slash</a></li>
-                    <li><a href="#">Sites</a></li>
-                    <li><a href="#">Foundation</a></li>
-                    <li><a href="#">Dot</a></li>
-                    <li><a href="#">ZURB</a></li>
-                    <li><a href="#">Com</a></li>
-                    <li><a href="#">Slash</a></li>
-                    <li><a href="#">Sites</a></li>
-                    <li><a href="#">Foundation</a></li>
-                    <li><a href="#">Dot</a></li>
-                    <li><a href="#">ZURB</a></li>
-                    <li><a href="#">Com</a></li>
-                    <li><a href="#">Slash</a></li>
-                    <li><a href="#">Sites</a></li>
-                    <li><a href="#">Foundation</a></li>
-                    <li><a href="#">Dot</a></li>
-                    <li><a href="#">ZURB</a></li>
-                    <li><a href="#">Com</a></li>
-                    <li><a href="#">Slash</a></li>
-                    <li><a href="#">Sites</a></li>
-                    <li><a href="#">Foundation</a></li>
-                    <li><a href="#">Dot</a></li>
-                    <li><a href="#">ZURB</a></li>
-                    <li><a href="#">Com</a></li>
-                    <li><a href="#">Slash</a></li>
-                    <li><a href="#">Sites</a></li>
-                </ul>
+        </div>
+    </header>
+    <div class="-corpo expanded row">
+        <div id="dm-menu-esquerdo" class="column">
+            <div id="menu-esquerdo-interno">
+                <div id="menu-esquerdo-interno-interno">
+                    <ul class="vertical menu">
+                        <li><a href="#">Foundation</a></li>
+                        <li><a href="#">Dot</a></li>
+                        <li><a href="#">ZURB</a></li>
+                        <li><a href="#">Com</a></li>
+                        <li><a href="#">Slash</a></li>
+                        <li><a href="#">Sites</a></li>
+                        <li><a href="#">Foundation</a></li>
+                        <li><a href="#">Dot</a></li>
+                        <li><a href="#">ZURB</a></li>
+                        <li><a href="#">Com</a></li>
+                        <li><a href="#">Slash</a></li>
+                        <li><a href="#">Sites</a></li>
+                        <li><a href="#">Foundation</a></li>
+                        <li><a href="#">Dot</a></li>
+                        <li><a href="#">ZURB</a></li>
+                        <li><a href="#">Com</a></li>
+                        <li><a href="#">Slash</a></li>
+                        <li><a href="#">Sites</a></li>
+                        <li><a href="#">Foundation</a></li>
+                        <li><a href="#">Dot</a></li>
+                        <li><a href="#">ZURB</a></li>
+                        <li><a href="#">Com</a></li>
+                        <li><a href="#">Slash</a></li>
+                        <li><a href="#">Sites</a></li>
+                        <li><a href="#">Foundation</a></li>
+                        <li><a href="#">Dot</a></li>
+                        <li><a href="#">ZURB</a></li>
+                        <li><a href="#">Com</a></li>
+                        <li><a href="#">Slash</a></li>
+                        <li><a href="#">Sites</a></li>
+                        <li><a href="#">Foundation</a></li>
+                        <li><a href="#">Dot</a></li>
+                        <li><a href="#">ZURB</a></li>
+                        <li><a href="#">Com</a></li>
+                        <li><a href="#">Slash</a></li>
+                        <li><a href="#">Sites</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="dm-painel column end">
-        <div class="expanded row">
-            <div id="dm-main" class="column">
-                <p>Teste do painel de conteudo</p>
+        <div class="dm-painel column end">
+            <div class="expanded row">
+                <div id="dm-main" class="column">
+                    <div class="small-2 column">
+                        <p>Teste do painel de conteudo</p>
+                    </div>
+                    <div class="small-2 column">
+                        <p>Teste do painel de conteudo</p>
+                    </div>
+                    <div class="small-2 column">
+                        <p>Teste do painel de conteudo</p>
+                    </div>
+                    <div class="small-2 column">
+                        <p>Teste do painel de conteudo</p>
+                    </div>
+                    <div class="small-2 column">
+                        <p>Teste do painel de conteudo</p>
+                    </div>
+                    <div class="small-2 column">
+                        <p>Teste do painel de conteudo</p>
+                    </div>
+                </div>
             </div>
+
         </div>
-        
     </div>
 </div>

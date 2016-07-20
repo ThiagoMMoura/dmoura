b2f84c26ea97ed5922b3eb20f63b3783001abce4
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-07-20 15:38:05 -0300

    [INC] Testes de layout

--- application/views/sistema/teste.php
+++ application/views/sistema/teste.php
@@ -1,84 +1,47 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<div class="dm-estrutura">
-    <header class="dm-cabecalho expanded row">
-        <div class="medium-6 column">
-            Teste
-        </div>
-        <div class="medium-6 column">
-
-        </div>
-    </header>
-    <div class="-corpo expanded row">
-        <div id="dm-menu-esquerdo" class="column">
-            <div id="menu-esquerdo-interno">
-                <div id="menu-esquerdo-interno-interno">
-                    <ul class="vertical menu">
-                        <li><a href="#">Foundation</a></li>
-                        <li><a href="#">Dot</a></li>
-                        <li><a href="#">ZURB</a></li>
-                        <li><a href="#">Com</a></li>
-                        <li><a href="#">Slash</a></li>
-                        <li><a href="#">Sites</a></li>
-                        <li><a href="#">Foundation</a></li>
-                        <li><a href="#">Dot</a></li>
-                        <li><a href="#">ZURB</a></li>
-                        <li><a href="#">Com</a></li>
-                        <li><a href="#">Slash</a></li>
-                        <li><a href="#">Sites</a></li>
-                        <li><a href="#">Foundation</a></li>
-                        <li><a href="#">Dot</a></li>
-                        <li><a href="#">ZURB</a></li>
-                        <li><a href="#">Com</a></li>
-                        <li><a href="#">Slash</a></li>
-                        <li><a href="#">Sites</a></li>
-                        <li><a href="#">Foundation</a></li>
-                        <li><a href="#">Dot</a></li>
-                        <li><a href="#">ZURB</a></li>
-                        <li><a href="#">Com</a></li>
-                        <li><a href="#">Slash</a></li>
-                        <li><a href="#">Sites</a></li>
-                        <li><a href="#">Foundation</a></li>
-                        <li><a href="#">Dot</a></li>
-                        <li><a href="#">ZURB</a></li>
-                        <li><a href="#">Com</a></li>
-                        <li><a href="#">Slash</a></li>
-                        <li><a href="#">Sites</a></li>
-                        <li><a href="#">Foundation</a></li>
-                        <li><a href="#">Dot</a></li>
-                        <li><a href="#">ZURB</a></li>
-                        <li><a href="#">Com</a></li>
-                        <li><a href="#">Slash</a></li>
-                        <li><a href="#">Sites</a></li>
-                    </ul>
-                </div>
+<div class="top-bar-wrapper">
+    <div class="top-bar-wrapper-inner is-open-top">
+        <div class="dm-top-bar position-top fundo-azul fonte-cinza-claro">
+            <div class="medium-6 column">
+                Coluna 1
             </div>
+            <div class="medium-6 column">
+                Coluna 2
+            </div>
+            <button type="button" class="button hide-for-large" data-toggle="offCanvas">Menu</button>
         </div>
-        <div class="dm-painel column end">
-            <div class="expanded row">
-                <div id="dm-main" class="column">
-                    <div class="small-2 column">
-                        <p>Teste do painel de conteudo</p>
+        <div class="top-bar-content fundo-azul-claro">
+            <div class="off-canvas-wrapper altura-maxima">
+                <div class="off-canvas-wrapper-inner altura-maxima is-off-canvas-open is-open-left" ><?php //data-off-canvas-wrapper ?>
+                    <div class="off-canvas position-left fundo-transparente is-open" aria-hidden="false" id="offCanvas" ><?php //data-off-canvas?>
+
+                        <!-- Close button -->
+                        <button class="close-button hide-for-large" aria-label="Close menu" type="button" data-close>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+
+                        <!-- Menu -->
+                        <ul class="vertical menu fonte-cinza-claro">
+                            <li><a href="#">Foundation</a></li>
+                            <li><a href="#">Dot</a></li>
+                            <li><a href="#">ZURB</a></li>
+                            <li><a href="#">Com</a></li>
+                            <li><a href="#">Slash</a></li>
+                            <li><a href="#">Sites</a></li>
+                        </ul>
+
                     </div>
-                    <div class="small-2 column">
-                        <p>Teste do painel de conteudo</p>
-                    </div>
-                    <div class="small-2 column">
-                        <p>Teste do painel de conteudo</p>
-                    </div>
-                    <div class="small-2 column">
-                        <p>Teste do painel de conteudo</p>
-                    </div>
-                    <div class="small-2 column">
-                        <p>Teste do painel de conteudo</p>
-                    </div>
-                    <div class="small-2 column">
-                        <p>Teste do painel de conteudo</p>
+
+                    <div class="off-canvas-content"><?php //data-off-canvas-content?>
+
+                        <div class="row expanded">
+                            Corpo
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </div>

5ad4be101e38c0afd57435af65e53f7da4a987c1
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-07-27 15:42:21 -0300

    [INC] Layout, Menus, barra principal, cores de fundo, busca menus, barra ferramentas

--- application/config/sistema.php
+++ application/config/sistema.php
@@ -10,6 +10,7 @@ $config['head-itens-antes-todos'] = array(
     array('meta'=>array('name'=>'x-ua-compatible','type'=>'http-equiv','content'=>'ie=edge')),
     array('meta'=>array('name'=>'viewport','type'=>'name','content'=>'width=device-width, initial-scale=1.0')),
     array('link'=>array('rel'=>'stylesheet','href'=>'assets/framework/css/foundation.min.css')),
+    array('link'=>array('rel'=>'stylesheet','href'=>'assets/framework/foundation-icons.css')),
     array('link'=>array('rel'=>'stylesheet','href'=>'assets/css/sistema.css')),
     array('script'=>array('src'=>'assets/framework/js/vendor/jquery.js'))
 );
--- application/views/sistema/body_base.php
+++ application/views/sistema/body_base.php
@@ -4,32 +4,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="top-bar-wrapper">
     <div class="top-bar-wrapper-inner is-open-top">
         <div id="barra-dmoura" class="dm-top-bar position-top fundo-azul fonte-branco">
-            <div class="medium-6 column">
-                <div class="dm-logotipo">
-                    <?php echo img('assets/imagens/DMOURA.png');?>
-                </div>
-            </div>
-            <div class="medium-6 column">
-                <div class="dm-usuario">
-                    <div class="dm-usuario-foto text-center">
-                        <?php echo img('assets/imagens/user.png');?>
-                    </div>
-                    <div class="dm-usuario-descricao text-center">
-                        <div class="dm-usuario-nome">
-                            Marlene Mariano de Moura
-                        </div>
-                        <div class="dm-usuario-cargo hide-for-small-only fonte-cinza-claro">
-                            <small>Gerente Administrativo</small>
-                        </div>
-                    </div>
-                    <div class="dm-usuario-menu">
-                        
+            <div class="barra-principal">
+                <div class="small-6 column">
+                    <div class="dm-logotipo">
+                        <?php echo img('assets/imagens/DMOURA.png');?>
                     </div>
                 </div>
+                <div class="small-6 column">
+                    <div class="dm-usuario">
+                        <div class="dm-usuario-foto">
+                            <?php echo img('assets/imagens/user.png');?>
+                        </div>
+                        <div class="dm-usuario-descricao hide-for-small-only">
+                            <div class="dm-usuario-nome">
+                                Marlene Mariano de Moura
+                            </div>
+                            <div class="dm-usuario-cargo hide-for-small-only fonte-cinza-claro">
+                                <small>Gerente Administrativo</small>
+                            </div>
+                        </div>
+                        <div class="dm-usuario-menu">
+
+                        </div>
+                    </div>
+                </div>
             </div>
-            <button type="button" class="button hide-for-large" data-toggle="offCanvas">Menu</button>
+            <div class="barra-ferramentas fundo-azul-8">
+                <div class="busca-menu">
+                    <button type="button" class="button hide-for-large fundo-azul-4" data-toggle="offCanvas"><i class="fi-list"></i></button>
+                    <input type="search" class="fundo-azul-4 show-for-large" name="in-busca-menu" placeholder="Pesquisa menus">
+                    <i class="fi-magnifying-glass show-for-large"></i>
+                </div>
+                <div class="barra-ferramentas-interna">
+                    <div class="barra-ferramentas-esquerda">
+                        <ul class="menu ">
+                            <li><a href="#">Deletar</a></li>
+                            <li><a href="#">Buscar</a></li>
+                            <li><a href="#">Salvar</a></li>
+                        </ul>
+                    </div>
+                    <div class="barra-ferramentas-direita">
+                        Titulo
+                    </div>
+                </div>
+            </div>
+            
         </div>
-        <div class="top-bar-content fundo-azul">
+        <div class="top-bar-content fundo-azul-1">
             <div class="off-canvas-wrapper altura-maxima">
                 <div class="off-canvas-wrapper-inner altura-maxima" data-off-canvas-wrapper>
                     <div class="off-canvas position-left fundo-transparente " data-reveal-class="reveal-for-large" data-close-on-click="false" id="offCanvas" data-off-canvas>
@@ -40,7 +61,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </button>-->
 
                         <!-- Menu -->
-                        <div class="fundo-azul-4" style="padding: 0.7em 1em;"><?php echo $titulo;?></div>
+                        <div class="fundo-azul-4 hide-for-large" style="padding: 0.7em 1em;"></div>
                         <?php echo imprimir_menu($this->config->item('menu-principal'),'drilldown');?>
                     </div>
 

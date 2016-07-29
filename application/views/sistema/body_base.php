4285ddfed2c3bf959c5e42c4e555616efc648a5f
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-07-29 15:37:34 -0300

    [INC] Menus, barra principal, busca menus, barra ferramentas.

--- application/views/sistema/body_base.php
+++ application/views/sistema/body_base.php
@@ -3,15 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="top-bar-wrapper">
     <div class="top-bar-wrapper-inner is-open-top">
-        <div id="barra-dmoura" class="dm-top-bar position-top fonte-branco">
-            <div class="barra-principal">
-                <div class="small-6 column">
+        <div id="barra-dmoura" class="dm-top-bar position-top">
+            <div class="barra-principal fonte-branco">
+                <div class="small-7 medium-4 column">
                     <div class="dm-logotipo">
                         <?php echo img('assets/imagens/DMOURA.png');?>
                     </div>
                 </div>
-                <div class="small-6 column">
-                    <div class="dm-usuario">
+                <div class="small-5 medium-8 column">
+                    <div class="dm-usuario float-right" data-toggle="dm-usuario-menu">
                         <div class="dm-usuario-foto">
                             <?php echo img('assets/imagens/user.png');?>
                         </div>
@@ -19,50 +19,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <div class="dm-usuario-nome">
                                 Marlene Mariano de Moura
                             </div>
-                            <div class="dm-usuario-cargo hide-for-small-only fonte-cinza-claro">
+                            <div class="dm-usuario-cargo fonte-cinza-claro">
                                 <small>Gerente Administrativo</small>
                             </div>
                         </div>
-                        <div class="dm-usuario-menu">
-
-                        </div>
+                    </div>
+                    <div class="dropdown-pane bottom" id="dm-usuario-menu" data-dropdown data-auto-focus="true" data-close-on-click="true">
+                        <ul class="menu">
+                            <li><a href="#">Sair</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <div class="barra-ferramentas">
                 <div class="busca-menu">
                     <button type="button" class="button hide-for-large" data-toggle="offCanvas"><i class="fi-list"></i></button>
-                    <input type="search" class="show-for-large" name="in-busca-menu" placeholder="Pesquisar Menu">
+                    <input type="search" class="show-for-large" name="bf-busca-menu" placeholder="Pesquisar Menu">
                     <i class="fi-magnifying-glass show-for-large"></i>
                 </div>
                 <div class="barra-ferramentas-interna">
-                    <div class="barra-ferramentas-esquerda">
-                        <ul class="menu ">
-                            <li><a href="#">Deletar</a></li>
-                            <li><a href="#">Buscar</a></li>
-                            <li><a href="#">Salvar</a></li>
-                        </ul>
-                    </div>
-                    <div class="barra-ferramentas-direita">
-                        Titulo
+                    <div class="barra-ferramentas-interna-conteudo">
+                        <div class="barra-ferramentas-esquerda">
+                            <ul class="menu ">
+                                <li class="menu-text">Titulo</li>
+                                <li><a href="#">Buscar</a></li>
+                                <li><a href="#">Salvar</a></li>
+                            </ul>
+                        </div>
+                        <div class="barra-ferramentas-direita">
+                            <ul class="menu ">
+                                <li><label for="salvar"><i class="fi-save"></i><span>Salvar</span></label></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-            
         </div>
         <div class="top-bar-content">
             <div class="off-canvas-wrapper altura-maxima">
                 <div class="off-canvas-wrapper-inner altura-maxima" data-off-canvas-wrapper>
                     <div class="off-canvas position-left" data-reveal-class="reveal-for-large" data-close-on-click="false" id="offCanvas" data-off-canvas>
-
-                        <!-- Close button 
-                        <button class="close-button hide-for-large" aria-label="Fechar menu" type="button" data-close>
-                            <span aria-hidden="true">&times;</span>
-                        </button>-->
-
+                        <!-- Busca -->
+                        <div class="off-canvas-busca hide-for-large">
+                            <input type="search" name="of-busca-menu" placeholder="Pesquisar Menu">
+                            <i class="fi-magnifying-glass"></i>
+                        </div>
                         <!-- Menu -->
-                        <div class="fundo-azul-4 hide-for-large" style="padding: 0.7em 1em;"></div>
-                        <?php echo imprimir_menu($this->config->item('menu-principal'),'drilldown');?>
+                        <div class="off-canvas-menu">
+                            <?php echo imprimir_menu($this->config->item('menu-principal'),'drilldown');?>
+                        </div>
                     </div>
 
                     <div id="dmoura-conteudo" class="off-canvas-content" data-off-canvas-content>

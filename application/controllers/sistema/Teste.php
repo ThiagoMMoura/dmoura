b16b8aa2ffe3563402b8aa9f9e5124a6777bf6f5
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-08-10 14:46:33 -0300

    [INC] Teste.

--- application/controllers/sistema/Teste.php
+++ application/controllers/sistema/Teste.php
@@ -13,11 +13,16 @@ class Teste extends MY_Controller{
     
     public function index(){
         $this->_add_data('nome','Thiago');
-        $this->_view("Formulário",'formulario',parent::RELATIVO_CONTROLE);
+        $this->_view("Teste index",'teste',parent::RELATIVO_CONTROLE);
     }
     
     public function formulario(){
         $this->_add_body('formulario',parent::RELATIVO_CONTROLE);
         $this->_view('Formulario');
+    }
+    
+    public function outro(){
+        $this->_add_data('nome','João');
+        $this->_view("Teste outro",'teste',parent::RELATIVO_CONTROLE);
     }
 }

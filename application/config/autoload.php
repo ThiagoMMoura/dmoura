918fbe13375a9368dab25264a2e4458928670afd
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-08-26 15:29:35 -0300

    [INC] Configurações gerais

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('url','html','head','body');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('geral');
 
 /*
 | -------------------------------------------------------------------
---
+++ application/config/geral.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Lista de configurações gerais.
+ *
+ * @author Thiago Moura
+ */
+$config['grupo_padrao_cliente'] = 1;
+
+/***********************************************************
+ * Dados da Empresa
+ ***********************************************************/
+$config['nome_fantasia'] = "D'moura";
+$config['email_suporte'] = 'suporte@dmoura.com.br';
+$config['email_contato'] = 'contato@dmoura.com.br';
+$config['email_vendas'] = 'vendas@dmoura.com.br';
+$config['email_admin'] = 'administrador@dmoura.com.br';
+

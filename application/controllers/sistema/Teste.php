aede023e061c8786d89e117ab40748aaf089c073
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-08-26 15:31:46 -0300

    [INC] Teste email e calendário

--- application/controllers/sistema/Teste.php
+++ application/controllers/sistema/Teste.php
@@ -12,7 +12,7 @@ class Teste extends MY_Controller{
     }
     
     public function index(){
-        $this->_add_data('nome',$this->controle_acesso->controle('sistema'));
+        $this->_add_data('nome',$this->load->view('sistema/email_padrao/cadastro_cliente',array('senha'=>'XhjbheruY','nome'=>'Thiago Moura'),TRUE));
         $this->_view("Teste index",'teste',parent::RELATIVO_CONTROLE);
     }
     
@@ -21,26 +21,45 @@ class Teste extends MY_Controller{
         $this->_view('Formulario');
     }
     
-    public function outro(){
-//        $this->load->library('xmlrpc');
-//
-//        $this->xmlrpc->server('http://viacep.com.br', 80);
-//        $this->xmlrpc->method('ws');
-//
-//        $request = array("94085150", 'json');
-//        $this->xmlrpc->request($request);
-//
-//        if ( ! $this->xmlrpc->send_request())
-//        {
-//                echo $this->xmlrpc->display_error();
-//        }
-//        $this->_add_data('nome',print_r($this->xmlrpc->display_response(),TRUE));
+    public function calendario(){
+        $prefs = array(
+            'month_type'   => 'long',
+            'day_type'     => 'long',
+            'show_next_prev'  => TRUE,
+            'next_prev_url'   => base_url($this->_obter_caminho_controle().'/calendario/')
+        );
+        $this->load->library('calendar',$prefs);
+        $data = array(
+            3  => 'http://example.com/news/article/2006/06/03/',
+            7  => 'http://example.com/news/article/2006/06/07/',
+            13 => 'http://example.com/news/article/2006/06/13/',
+            26 => 'http://example.com/news/article/2006/06/26/'
+        );
+        $this->_add_data('nome', $this->calendar->generate($this->uri->segment(4), $this->uri->segment(5),$data));
+        
         $this->_view("Teste outro",'teste',parent::RELATIVO_CONTROLE);
     }
     
     public function senha($senha){
         $senha = md5(trim($senha));
         $this->_add_data('nome', $senha);
+        $this->_view("Teste outro",'teste',parent::RELATIVO_CONTROLE);
+    }
+    
+    public function gerar_string(){
+        $this->load->helper('string');
+        $senha = random_string('alnum', 8);
+        $this->_add_data('nome',$senha);
+        
+        $this->load->library('email');
+
+        $this->email->from('dmouracontato@bol.com.br', "D'Moura");
+        $this->email->to('ago10_mariano@yahoo.com.br');
+
+        $this->email->subject('Email Test');
+        $this->email->message('Testing the email class.' . $senha);
+
+        $this->email->send();
         $this->_view("Teste outro",'teste',parent::RELATIVO_CONTROLE);
     }
 }

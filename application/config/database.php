7d2e6dc1d339ad2ec0ea07c1abd8897244b35e96
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-08-26 15:28:30 -0300

    [INC] Configurações banco de dados

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,24 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+$db['hostinger'] = array(
+    'dsn'	=> '',
+    'hostname' => 'mysql.hostinger.com.br',
+    'username' => 'u531429788_dm',
+    'password' => 'JpcYGkha0B',
+    'database' => 'u531429788_dm',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);

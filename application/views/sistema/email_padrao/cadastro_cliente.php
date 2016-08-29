6b4c052612c5a2d834c9a0684ad3b81d27a324fe
61171 <61171@3A-AD-D-08.ascar.tche.br>
2016-08-29 09:51:14 -0300

    [INC] Teste email, template

--- application/views/sistema/email_padrao/cadastro_cliente.php
+++ application/views/sistema/email_padrao/cadastro_cliente.php
@@ -1,13 +1,15 @@
-Olá, <?php echo $nome;?>
+<p>Olá, <?php echo $nome;?></p>
 
-Seu cadastro foi efetuado com sucesso.
-
+<p>Seu cadastro foi efetuado com sucesso.</p>
+<br/>
+<p>
 <?php if(isset($senha)){?>
-Sua senha de acesso é: <font style="color: green;"><?php echo $senha;?></font>
+Sua senha de acesso é: <font style="color: green;font-weight: bold"><?php echo $senha;?></font><br/>
 <?php } ?>
 Para sua segurança, em seu primeiro login você será redirecionado para a criação de uma nova senha.
-
-Atenciosamente,
-Equipe <?php echo config_item('nome_fantasia');?>
-<?php echo config_item('email_contato');
- 
+</p>
+<p>
+    Atenciosamente,<br/>
+    Equipe <?php echo config_item('nome_fantasia');?><br/>
+    <?php echo config_item('email_contato');?><br/>
+</p>

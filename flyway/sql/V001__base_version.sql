51dfced941c9460877100aa1c1400ea71dfc8a2c
ThiagoMMoura <ago10_mariano@yahoo.com.br>
2016-12-23 22:08:19 -0200

    [INC] Configuração do Flyway e migration base

---
+++ flyway/sql/V001__base_version.sql
@@ -0,0 +1,454 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 24-Dez-2016 às 00:34
+-- Versão do servidor: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dmoura`
+--
+CREATE DATABASE IF NOT EXISTS `dmoura` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dmoura`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `bairro`
+--
+
+DROP TABLE IF EXISTS `bairro`;
+CREATE TABLE IF NOT EXISTS `bairro` (
+  `id` int(11) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `municipio` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ci_sessions`
+--
+
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `id` varchar(40) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+CREATE TABLE IF NOT EXISTS `endereco` (
+  `cep` int(8) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `municipio` int(11) DEFAULT NULL,
+  `bairro` int(11) DEFAULT NULL,
+  `logradouro` int(11) DEFAULT NULL,
+  `num_ini` int(11) DEFAULT '0',
+  `num_fim` int(11) DEFAULT '0',
+  `lado` tinyint(1) DEFAULT NULL,
+  `complemento` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estado`
+--
+
+DROP TABLE IF EXISTS `estado`;
+CREATE TABLE IF NOT EXISTS `estado` (
+  `uf` varchar(2) NOT NULL,
+  `nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `estado`
+--
+
+INSERT INTO `estado` (`uf`, `nome`) VALUES
+('AC', 'Acre'),
+('AL', 'Alagoas'),
+('AP', 'Amapá'),
+('AM', 'Amazonas'),
+('BA', 'Bahia'),
+('CE', 'Ceará'),
+('DF', 'Distrito Federal'),
+('ES', 'Espírito Ssnto'),
+('GO', 'Goiás'),
+('MA', 'Maranhão'),
+('MT', 'Mato Grosso'),
+('MS', 'Mato Grosso do Sul'),
+('MG', 'Minas Gerais'),
+('PA', 'Pará'),
+('PB', 'Paraíba'),
+('PR', 'Paraná'),
+('PE', 'Pernambuco'),
+('PI', 'Piauí'),
+('RJ', 'Rio de Janeiro'),
+('RN', 'Rio Grande do Norte'),
+('RS', 'Rio Grande do Sul'),
+('RO', 'Rondonia'),
+('RR', 'Roraima'),
+('SC', 'Santa Catarina'),
+('SP', 'São Paulo'),
+('SE', 'Sergipe'),
+('TO', 'Tocantins');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id` int(11) NOT NULL,
+  `rg` varchar(10) NOT NULL,
+  `cargo` int(11) NOT NULL,
+  `salario` float NOT NULL DEFAULT '0',
+  `pessoa_fisica` int(11) NOT NULL,
+  `ferias` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `grupo_permissoes`
+--
+
+DROP TABLE IF EXISTS `grupo_permissoes`;
+CREATE TABLE IF NOT EXISTS `grupo_permissoes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `descricao` varchar(250) DEFAULT NULL,
+  `fixo` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `logradouro`
+--
+
+DROP TABLE IF EXISTS `logradouro`;
+CREATE TABLE IF NOT EXISTS `logradouro` (
+  `id` int(11) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `municipio` int(11) NOT NULL,
+  `nome` varchar(150) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `municipio`
+--
+
+DROP TABLE IF EXISTS `municipio`;
+CREATE TABLE IF NOT EXISTS `municipio` (
+  `id` int(11) NOT NULL,
+  `uf` varchar(2) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `operadora_telefone`
+--
+
+DROP TABLE IF EXISTS `operadora_telefone`;
+CREATE TABLE IF NOT EXISTS `operadora_telefone` (
+  `id` int(11) NOT NULL,
+  `operadora` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `operadora_telefone`
+--
+
+INSERT INTO `operadora_telefone` (`id`, `operadora`) VALUES
+(1, 'Claro'),
+(2, 'Oi'),
+(3, 'Tim'),
+(4, 'Vivo');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `permissao`
+--
+
+DROP TABLE IF EXISTS `permissao`;
+CREATE TABLE IF NOT EXISTS `permissao` (
+  `id` int(11) NOT NULL,
+  `grupo` int(11) NOT NULL,
+  `area` int(11) NOT NULL,
+  `liberado` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa`
+--
+
+DROP TABLE IF EXISTS `pessoa`;
+CREATE TABLE IF NOT EXISTS `pessoa` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `cep` varchar(8) DEFAULT NULL,
+  `numero` int(5) NOT NULL DEFAULT '0',
+  `complemento` varchar(100) DEFAULT NULL,
+  `senha` varchar(150) NOT NULL,
+  `grupo` int(11) NOT NULL,
+  `tipo` tinyint(4) NOT NULL DEFAULT '1',
+  `tel_principal` int(11) DEFAULT '0',
+  `resenha` tinyint(1) NOT NULL DEFAULT '1',
+  `ativo` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pessoa`
+--
+
+INSERT INTO `pessoa` (`id`, `nome`, `email`, `cep`, `numero`, `complemento`, `senha`, `grupo`, `tipo`, `tel_principal`, `resenha`, `ativo`) VALUES
+(1, 'WebMaster', 'webmaster@dmoura.com.br', NULL, 0, NULL, '1c9e899ab77610223649760332ddfee6ec0a9ab1', 1, 1, NULL, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa_fisica`
+--
+
+DROP TABLE IF EXISTS `pessoa_fisica`;
+CREATE TABLE IF NOT EXISTS `pessoa_fisica` (
+  `id` int(11) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `nascimento` date NOT NULL,
+  `sexo` enum('masculino','feminino','outro','') NOT NULL,
+  `nacionalidade` varchar(100) DEFAULT NULL,
+  `naturalidade` varchar(100) DEFAULT NULL,
+  `estado_civil` varchar(50) DEFAULT NULL,
+  `pessoa` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `telefone`
+--
+
+DROP TABLE IF EXISTS `telefone`;
+CREATE TABLE IF NOT EXISTS `telefone` (
+  `id` int(11) NOT NULL,
+  `ddd` int(2) NOT NULL,
+  `telefone` int(11) NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `operadora` int(11) NOT NULL,
+  `pessoa` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo_telefone`
+--
+
+DROP TABLE IF EXISTS `tipo_telefone`;
+CREATE TABLE IF NOT EXISTS `tipo_telefone` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tipo_telefone`
+--
+
+INSERT INTO `tipo_telefone` (`id`, `tipo`) VALUES
+(1, 'Casa'),
+(2, 'Celular'),
+(3, 'Fax'),
+(4, 'Trabalho'),
+(5, 'WhatsApp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bairro`
+--
+ALTER TABLE `bairro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD KEY `ci_sessions_timestamp` (`timestamp`);
+
+--
+-- Indexes for table `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`cep`);
+
+--
+-- Indexes for table `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`uf`),
+  ADD UNIQUE KEY `un_nome` (`nome`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `un_rg` (`rg`);
+
+--
+-- Indexes for table `grupo_permissoes`
+--
+ALTER TABLE `grupo_permissoes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `un_nome` (`nome`);
+
+--
+-- Indexes for table `logradouro`
+--
+ALTER TABLE `logradouro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `municipio`
+--
+ALTER TABLE `municipio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `operadora_telefone`
+--
+ALTER TABLE `operadora_telefone`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `un_operadora` (`operadora`);
+
+--
+-- Indexes for table `permissao`
+--
+ALTER TABLE `permissao`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pessoa`
+--
+ALTER TABLE `pessoa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `un_email` (`email`);
+
+--
+-- Indexes for table `pessoa_fisica`
+--
+ALTER TABLE `pessoa_fisica`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cpf_2` (`cpf`),
+  ADD KEY `in_cpf` (`cpf`),
+  ADD KEY `cpf` (`cpf`);
+
+--
+-- Indexes for table `telefone`
+--
+ALTER TABLE `telefone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tipo_telefone`
+--
+ALTER TABLE `tipo_telefone`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `un_tipo` (`tipo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bairro`
+--
+ALTER TABLE `bairro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `grupo_permissoes`
+--
+ALTER TABLE `grupo_permissoes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `logradouro`
+--
+ALTER TABLE `logradouro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT for table `municipio`
+--
+ALTER TABLE `municipio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `operadora_telefone`
+--
+ALTER TABLE `operadora_telefone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `permissao`
+--
+ALTER TABLE `permissao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pessoa`
+--
+ALTER TABLE `pessoa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `pessoa_fisica`
+--
+ALTER TABLE `pessoa_fisica`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `telefone`
+--
+ALTER TABLE `telefone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tipo_telefone`
+--
+ALTER TABLE `tipo_telefone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

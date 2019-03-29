915492dd43fbc35e96d8aa31c608baeacfc6bb47
Thiago Moura <ago10_mariano@yahoo.com.br>
2019-03-29 17:12:08 -0300

    Update input-string property (ngModel)

--- src/app/util/fields/input-string/input-string.component.ts
+++ src/app/util/fields/input-string/input-string.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import {
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+import { ValueAccessorBase } from '../value-accessor-base';
 
 @Component({
   selector: 'app-input-string',
   templateUrl: './input-string.component.html',
-  styleUrls: ['./input-string.component.scss']
+  styleUrls: ['./input-string.component.scss'],
+  providers: [
+    {provide: NG_VALUE_ACCESSOR, useExisting: InputStringComponent, multi: true}
+  ],
 })
-export class InputStringComponent implements OnInit {
+export class InputStringComponent extends ValueAccessorBase<string> implements OnInit {
+  @Input()
+  disabled: boolean;
 
-  constructor() { }
+  @Input()
+  errorMensage: string;
+
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
--- src/app/util/fields/util-fields.module.ts
+++ src/app/util/fields/util-fields.module.ts
@@ -1,13 +1,15 @@
 import { InputStringComponent } from './input-string/input-string.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     InputStringComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     InputStringComponent
